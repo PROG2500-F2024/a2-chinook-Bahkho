cee438883b628f50501309d3f7144be8214a3ab8
Abdulwahab Bakare <akoredebakare4u@gmail.com>
2024-11-06 09:08:24 -0400

    Assignment 2 finished

--- PROG2500-A2-Chinook/App.xaml.cs
+++ PROG2500-A2-Chinook/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Windows.UI;
 
 namespace PROG2500_A2_Chinook
 {
